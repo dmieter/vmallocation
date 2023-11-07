e12009c4de2e3b7ad18c5ff5b4098bd01568dbf7
dmieter <32790897+dmieter@users.noreply.github.com>
2023-11-07 13:16:23 +0300

    New code with threaded library call

--- py/vmallocation.py
+++ py/vmallocation.py
@@ -4,13 +4,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch    
+from random import randrange
 
 print("Import Successful")
 
 # %% LOAD INPUT DATA
 
 ##### LOAD TASKS ##### 
-def loadTasks():
+def loadTasksLocal():
     tasks = pd.read_csv('tasks.csv', delimiter=';')
     data_transfers = pd.read_csv('datatransfers.csv', delimiter=';')
 
@@ -27,10 +28,70 @@ def loadTasks():
 
     return tasks, data_transfers
 
+def loadTasksMontageV1():
+    tasks = pd.read_csv('montage_v1/task_time_table.csv', delimiter=',')
+    tasks["task_id"]=tasks["task_id"].values.astype('str')
+    tasks['task'] = tasks['task_id'] + "/" + tasks['task_name']
+    tasks['start'] = tasks['start']*10  # cause some tasks have < 1 interval
+    tasks['end'] = tasks['end']*10
+    tasks['volume'] = (tasks.end - tasks.start) * 0.8
+    data_transfers = pd.read_csv('montage_v1/transfer_size_table.csv', delimiter=',')
+
+    tasks['index'] = tasks['task']
+    tasks.set_index('index', inplace = True)
+
+    tasks["task_type"] = 'Task'
+    tasks["task_status"] = 'None'
+    tasks["assigned_vm"] = pd.NA
+    tasks["allocation_end"] = pd.NA
+
+    # days between start and end of each task
+    tasks['interval'] = tasks.end - tasks.start
+
+    return tasks, data_transfers
+
+def loadTasks(path = "workflows_v03/LIGO50/"):
+    tasks = pd.read_csv(path + "task_time_table.csv", delimiter=',')
+    tasks["task_id"]=tasks["task_id"].values.astype('str')
+    tasks['task'] = tasks['task_id'] + "/" + tasks['task_name']
+    tasks.rename(columns={'start_time':'start', 'finish_time':'end'}, inplace=True)
+    #tasks['start'] = tasks['start']*10  # cause some tasks have < 1 interval
+    #tasks['end'] = tasks['end']*10
+    #tasks['volume'] = tasks['volume']*10
+    data_transfers = pd.read_csv(path + "transfer_size_table.csv", delimiter=',')
+    data_transfers.rename(columns={'task_from':'source_task', 'task_to':'target_task', 'transfer_size': 'data_volume' }, inplace=True)
+
+    tasks['index'] = tasks['task']
+    tasks.set_index('index', inplace = True)
+
+    tasks["task_type"] = 'Task'
+    tasks["task_status"] = 'None'
+    tasks["assigned_vm"] = pd.NA
+    tasks["allocation_end"] = pd.NA
+
+    # days between start and end of each task
+    tasks['interval'] = tasks.end - tasks.start
+
+    return tasks, data_transfers
+
 ##### LOAD VMs ##### 
-def loadVMs():
+def loadVMsLocal():
     vm_counter = 0
     vm_types = pd.read_csv('vmtypes.csv', delimiter=';')
+    vms = pd.DataFrame()
+    print("VM TYPES:")
+    print(vm_types)
+
+    return vm_types, vms
+
+
+def loadVMs(path = "workflows_v03/LIGO50/"):
+    vm_counter = 0
+    vm_types = pd.read_csv(path + "processor_table.csv", delimiter=',')
+    vm_types.rename(columns={'performance':'perf', 'VM_type': "vm_type"}, inplace=True)
+    vm_types['cost'] = vm_types['perf']
+    vm_types['vm_start_time'] = 1
+    vm_types['vm_stop_time'] = 1
     vms = pd.DataFrame()
     print("VM TYPES:")
     print(vm_types)
@@ -78,6 +139,7 @@ def assignToLeader(vm_types, tasks, time, num):
 
 def retrieveParalellBatches(vm_types, tasks, time):
     batch_num = 0
+    tasks.loc[(tasks.task.str.contains('entry')) | (tasks.task.str.contains('finish')), 'task_status'] = 'IO'
     while('None' in tasks.task_status.values):
         time = assignToLeader(vm_types, tasks, time, batch_num)
         batch_num += 1
@@ -106,15 +168,21 @@ def generateVmId(row):
 
 def addNewVmsRandomly(vms, vm_types, num, vm_perf_factor = 1):
 
-    # 1. take more samples then required by factor of vm_perf_factor
-    initial_sample_num = num * vm_perf_factor if vm_perf_factor >= 1 else num / vm_perf_factor
-    initial_sample_num = math.ceil(initial_sample_num)
+    if vm_perf_factor < 10:
+        # 1. take more samples then required by factor of vm_perf_factor
+        initial_sample_num = num * vm_perf_factor if vm_perf_factor >= 1 else num / vm_perf_factor
+        initial_sample_num = math.ceil(initial_sample_num)
 
-    random_types = vm_types.sample(initial_sample_num, replace = True)
+        random_types = vm_types.sample(initial_sample_num, replace = True)
 
-    # 2. if needed - pick first n by perf
-    if vm_perf_factor != 1:
-        random_types = random_types.nlargest(num,'perf') if vm_perf_factor >= 1 else random_types.nsmallest(num,'perf')
+        # 2. if needed - pick first n by perf
+        if vm_perf_factor != 1:
+            random_types = random_types.nlargest(num,'perf') if vm_perf_factor >= 1 else random_types.nsmallest(num,'perf')
+
+    else:
+        # take all max perf VMs in case vm_perf_factor is really big
+        maxPerf = vm_types.perf.max()
+        random_types = vm_types[vm_types.perf == maxPerf].sample(num, replace = True)            
 
     random_types['vm_status'] = 'open'
     random_types['vm_id'] = random_types.apply(lambda x: generateVmId(x), axis=1)  
@@ -173,8 +241,8 @@ def prepareVmMatchings(batch, vms, additional_vms_num = 0, vm_perf_factor = 1):
 import random
 import math
 
-current_time = 0
-DATA_TRANSFER_SPEED = 50
+current_time = -10
+DATA_TRANSFER_SPEED = 50000000 #50
 
 def calcVmAllocationCost(row):
 
@@ -190,7 +258,8 @@ def calcVmAllocationCost(row):
     input_data_transfer = np.NaN
     output_data_transfer = np.NaN
 
-    
+    #if row.task == '49/ID00048':
+    #    print("TASK 49/ID00048")
 
     vm_type = vm_types[vm_types.vm_type == row.vm_type].iloc[0]
 
@@ -223,7 +292,8 @@ def calcVmAllocationCost(row):
     
     # if perform calculations
     else:
-        runtime = math.ceil(row.volume / vm_type.perf) 
+        #runtime = math.ceil(row.volume / vm_type.perf) 
+        runtime = row.volume / vm_type.perf 
         if(row.vm_status == 'open'):
             preparation_time = preparation_time + vm_type.vm_start_time             # add startup time
 
@@ -298,7 +368,8 @@ def calculateMinCostPairings(matchings):
     return pairings
 
 from munkres import Munkres, print_matrix, DISALLOWED
-def calculateMinCostPairings2(matchings):
+def calculateMinCostPairings2(matchings, resultArray = []):
+    print("calculateMinCostPairings2")
     unique_tasks = list(matchings.task.unique())
     unique_vms = list(matchings.vm_id.unique())
     cost_matrix = []
@@ -329,7 +400,56 @@ def calculateMinCostPairings2(matchings):
         matchings.loc[(matchings.task == task_name) & (matchings.vm_id == vm_id), 'pairing'] = 1
 
     pairings = matchings[matchings.pairing == 1]
+    print("calculateMinCostPairings2 setting result")
+    resultArray.append(pairings)
     return pairings
+
+    
+
+import ctypes
+from threading import Thread
+import time
+def terminate_thread(thread):
+    
+    exc = ctypes.py_object(SystemExit)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(thread.ident), exc)
+    if res == 0:
+        raise ValueError("nonexistent thread id")
+    elif res > 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(thread.ident, None)
+        
+def terminate_thread_with_retries(thread, retry_num=5):
+    retry_cnt = 0
+    while thread.is_alive():
+        terminate_thread(thread)
+        time.sleep(1)
+        retry_cnt = retry_cnt + 1
+        if retry_cnt >= retry_num:
+            print("CAN'T RAISE EXCEPTION IN THREAD THREAD")
+            break
+
+def calculateMinCostPairingsThreaded(matchings):
+    
+
+    matchingResult = []
+    thread = Thread(target=calculateMinCostPairings2, args=(matchings, matchingResult))
+    thread.start()
+    for t in range(1,4):
+        time.sleep(t)
+        print("Next check: {}".format(t)) 
+        if len(matchingResult) > 0:
+            break
+
+
+    if thread.is_alive():
+        terminate_thread_with_retries(thread)
+
+    return matchingResult[0] 
+
+
+
 
 
 
@@ -396,12 +516,15 @@ def scheduleBatch(batch_num, tasks, vms):
         try:
             matchings = prepareVmMatchings(batch, vms, 5, vm_perf_factor)
             calcAllocationCosts(matchings)
-            pairings = calculateMinCostPairings2(matchings)
+            pairings = calculateMinCostPairingsThreaded(matchings)
             break
         except Exception:
-            print(matchings[["task", 'volume', "vm_id", "allocation_cost", 'vm_start', 'task_start', 'task_end', 'vm_end']])
+            #print(matchings[["task", 'volume', "vm_id", 'perf', "allocation_cost", 'start', 'end,' 'task_start', 'task_end']])
             retry_counts = retry_counts - 1
-            vm_perf_factor += 0.25
+            vm_perf_factor += 1
+            if vm_perf_factor > 3:
+                vm_perf_factor = 100 
+            print("New vm_perf_factor: {}".format(vm_perf_factor))        
             INCREASE_IN_PERF_NUMBER = INCREASE_IN_PERF_NUMBER + 1
 
     #print(pairings[["task", 'volume', "vm_id", "allocation_cost", 'vm_start', 'task_start', 'task_end', 'vm_end', "pairing"]])
@@ -417,6 +540,11 @@ def runExperiment():
     vm_counter = 0
 
     retrieveParalellBatches(vm_types, tasks, 0)
+
+    # need entry task to support initial (entry) data transfers
+    #entry_task = {'task_id':'0', 'task':'0/entry', 'allocation_end': 0}
+    #tasks = tasks.append(entry_task, ignore_index=True)
+    
     tasks = tasks.sort_values(['finish_time', 'task_status', 'start'], ascending = [True, False, True])
 
     batches = list(tasks.batch.unique())
@@ -440,11 +568,14 @@ def runExperiment():
     DATA TRANSFERS:""")
     print(data_transfers)    
 
+
+    totalCost = vms_log.allocation_cost.sum()
+    totalInputDataTransfers = vms_log.input_data.sum()
     print("""
           
-    ALLOCATION LOG:""")
+    ALLOCATION LOG: {} {}""".format(totalCost, totalInputDataTransfers))
     print(vms_log)
-    print('INCREASE_IN_PERF_NUMBER: {}'.format(INCREASE_IN_PERF_NUMBER))
+    vms_log.to_csv("allocation_log.csv", sep=';')
 
 
 # %% TEST
